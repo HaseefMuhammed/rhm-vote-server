4db5a47a4054b0cb7c13cbfa781ff8a2dd8c6532
HaseefMuhammed <haseefmuhammedpc@gmail.com>
2024-05-27 14:54:26 +0530

    final file

---
+++ script.js
@@ -0,0 +1,8 @@
+// function vote() {
+//     // Play the audio clip
+//     var audio = new Audio('images/clip.mp3');
+//     audio.play();
+    
+//     // You can add additional functionality here if needed
+// }
+// console.log(`script ready`);
